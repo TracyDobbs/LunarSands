51bfcab5c4824b9027443b19abbec45862b16cc7
Tracy Dobbs <103674554+TracyDobbs@users.noreply.github.com>
2023-10-30 14:04:18 -0600

    Add files via upload

---
+++ interactables/LevelChangeZone.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# Variables
+# Components
+@onready var area2d = $Area2D
+
+# Level
+@export var levelName: String
+
+# Called when the player enters the zone
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		GlobalScript.change_level(levelName)
