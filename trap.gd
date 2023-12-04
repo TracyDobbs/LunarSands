db8452874838862f85da30a41cd50f3879dcc3ee
PartridgeKellen <143986002+PartridgeKellen@users.noreply.github.com>
2023-12-04 13:51:18 -0700

    Trap kills player depending on their state.
    The trap object kills the player only if they are human.

--- trap.gd
+++ trap.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
-
-
-
-
 func _on_body_entered(body):
-	if body.has_method("die"):
+	if body == null:
+		print("body is null")
+		return
+	if body.is_in_group("Human"):
 		body.die()
